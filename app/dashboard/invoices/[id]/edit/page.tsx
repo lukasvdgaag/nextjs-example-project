959f07b8bcd17e582d795c7a7a4559adcccc9e87
lukasvdgaag <lukasvdgaag@gmail.com>
2024-02-12 00:12:36 +0100

    Added metadata.

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,20 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import {fetchCustomers, fetchInvoiceById} from "@/app/lib/data";
 import {notFound} from "next/navigation";
+import {Metadata, ResolvingMetadata} from "next";
+
+export async function generateMetadata({params, searchParams}: {
+    params: { id: string },
+    searchParams: { [key: string]: string },
+}, parent: ResolvingMetadata): Promise<Metadata> {
+    const {id} = params;
+
+    const invoice = await fetchInvoiceById(id);
+
+    return {
+        title: `Edit Invoice ($${invoice.amount})`
+    }
+}
 
 export default async function Page({params}: { params: { id: string } }) {
     const id = params.id;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import React from "react";
 import {inter} from "@/app/ui/fonts";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard'
+    },
+    description: 'The official Next.js course dashboard, built with app router.',
+    metadataBase: new URL('https://next-learn-dashboard.vercel.app/')
+}
 
 export default function RootLayout({
                                        children,
